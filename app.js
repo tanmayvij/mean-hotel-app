3ad2bdde737e79b16d3500f2f692e236c5276713
Tanmay Vij <tanmayvij047695@gmail.com>
2019-04-25 21:22:33 +0530

    CORS Mod

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
 	console.log(req.method, req.url);
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE,OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token,Authorization');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
