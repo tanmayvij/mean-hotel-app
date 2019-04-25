d41d93b78344947b9f4cc1b4c27b6fbe73627049
Tanmay Vij <tanmayvij047695@gmail.com>
2019-04-25 21:14:53 +0530

    CORS Update

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.set('port', 8080);
 
 app.use(function(req, res, next) {
 	console.log(req.method, req.url);
-	res.header("Access-Control-Allow-Origin", "*");
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
 app.use(express.static(path.join(__dirname, 'public')));
