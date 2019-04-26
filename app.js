0a4b10d586bb4e4ea0d7ff9f72b1a365457ee711
Tanmay Vij <tanmayvij047695@gmail.com>
2019-04-26 13:16:07 +0530

    Body Parser update

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended : false }));
 app.use('/api', routes);
 
 var server = app.listen(app.get('port'), function() {
